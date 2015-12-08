5e2e015e5f5c90c752e31fe6879ce4d56be4a63d
Kevin Dice <kevin.dice1@gmail.com>
2015-12-08 00:22:20 -0600

    Extend job mem limit and time length
    As a precaution, the memory limit and time limit are extended

--- 1-extract-data/start-job.sh
+++ 1-extract-data/start-job.sh
@@ -1,2 +1,2 @@
 ./update-file-list.sh
-qsub -l mem=512M,h_rt=1:00:00 -q killable.q,batch.q -t 1-`cat filelist.txt | wc -l`:1 job.sh
+qsub -l mem=2G,h_rt=2:00:00 -q killable.q,batch.q -t 1-`cat filelist.txt | wc -l`:1 job.sh
