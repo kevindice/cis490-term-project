81bee9ae4ca473dfde0186312467593c84fa9a8b
Kevin Dice <kevin.dice1@gmail.com>
2015-12-08 16:11:19 -0600

    Add ETL cleaning test with tiny data sample

--- 1-extract-data/start-job.sh
+++ 1-extract-data/start-job.sh
@@ -1,2 +1,2 @@
 ./update-file-list.sh
-qsub -l mem=2G,h_rt=2:00:00 -q killable.q,batch.q -t 1-`cat filelist.txt | wc -l`:1 job.sh
+qsub -l mem=3G,h_rt=4:00:00 -q killable.q,batch.q -t 1-`cat filelist.txt | wc -l`:1 job.sh
---
+++ 2-test-with-sample/mapper.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python2.7
+import sys
+import json
+import unicodedata
+from string import punctuation
+from string import maketrans
+import re
+
+def clean(input):
+    v1 = unicodedata.normalize('NFC', input)
+
+    remap = {
+        ord('\t') : u' ',
+        ord('\f') : u' ',
+        ord('\r') : None,
+        ord('\n') : u' ',
+        ord('!'): None,
+        ord('!') : None,
+        ord('"') : None,
+        ord('#') : None,
+        ord('$') : None,
+        ord('%') : None,
+        ord('&') : None,
+        ord('\\') : None,
+        ord('\'') : None,
+        ord('(') : None,
+        ord(')') : None,
+        ord('*') : None,
+        ord('+') : None,
+        ord(',') : None,
+        ord('-') : None,
+        ord('.') : None,
+        ord('/') : None,
+        ord(':') : None,
+        ord(';') : None,
+        ord('<') : None,
+        ord('=') : None,
+        ord('>') : None,
+        ord('?') : None,
+        ord('@') : None,
+        ord('[') : None,
+        ord('\\') : None,
+        ord('\\') : None,
+        ord(']') : None,
+        ord('^') : None,
+        ord('_') : None,
+        ord('`') : None,
+        ord('{') : None,
+        ord('|') : None,
+        ord('}') : None,
+        ord('~') : None
+    }
+
+    v2 = v1.translate(remap)
+
+    v3 = re.sub("\s\s+", " ", v2)
+
+    v4 = v3.lower().strip()
+
+    return v4
+
+
+for line in sys.stdin:
+    post = json.loads(line)
+    print clean(post['body'])
+
