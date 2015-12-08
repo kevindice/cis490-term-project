2195f961da0683985c9ba5bc100f2654d16f68fc
Kevin Dice <kmdice@selene.beocat>
2015-12-07 22:14:33 -0600

    Renaming mistake

--- job.sh
+++ job.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#$ -t 1:15
-sh /homes/kmdice/cis490-term-project/ping.sh `sed -n "${SGE_TASK_ID}p" /homes/kmdice/cis490-term-project/filelist.txt`
+/homes/kmdice/cis490-term-project/ping.sh `sed -n "${SGE_TASK_ID}p" /homes/kmdice/cis490-term-project/filelist.txt`
